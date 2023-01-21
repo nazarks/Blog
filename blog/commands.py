06eb8d3ad59f93dff9d4d86c96f85e6753be1634
Sego <grayg@mail.ru>
2023-01-21 22:06:11 +0300

    lesson_7

--- blog/commands.py
+++ blog/commands.py
@@ -31,3 +31,25 @@ def create_init_users():
     db.session.add_all(users)
     db.session.commit()
     print(f"done! created users: { [user.username for user in users]}")
+
+
+@click.command("create-tags")
+def create_tags():
+    """
+    Run in your terminal:
+    flask create-tags
+    """
+    from blog.models import Tag
+
+    for name in [
+        "flask",
+        "django",
+        "python",
+        "sqlalchemy",
+        "news",
+    ]:
+        tag = Tag(name=name)
+        db.session.add(tag)
+    db.session.commit()
+
+    print("created tags")
--- blog/models/__init__.py
+++ blog/models/__init__.py
@@ -1,9 +1,11 @@
 from blog.models.article import Article
 from blog.models.author import Author
+from blog.models.tag import Tag
 from blog.models.user import User
 
 __all__ = [
     "User",
     "Author",
     "Article",
+    "Tag",
 ]
